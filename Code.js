7cd52275d48806c558297f7d789c213f7162eda0
yama <s256282@wakayama-u.ac.jp>
2022-06-15 20:27:26 +0900

    create Sum

--- Code.js
+++ Code.js
@@ -1,5 +1,6 @@
 const isValid = (s) => {
     let s_Number = [];
+
     //括弧を抽出し、それぞれを数字に変換して格納。
     for(let i in s){
         switch(s[i]){
@@ -12,7 +13,14 @@ const isValid = (s) => {
             default: break;
         }
     }
-    //括弧のルールが違えばその時点でfalseを返す。
+
+    //同じ鍵括弧で閉じられているか判別
+    let Sum = s_Number.reduce((a,b)=>{
+        return a + b 
+    })
+    if(Sum != 0) return false
+
+    //括弧が開いた順に閉じられているか判別。
     for(let i=0;i<s.length/2;i++) if(i%2 == s_Number.lastIndexOf(-s_Number[i])%2) return false;
     
     return true;
