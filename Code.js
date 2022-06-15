2acbf98eaff8bcd21e8ce226b5113c0f78a2930a
yama <s256282@wakayama-u.ac.jp>
2022-06-15 19:58:27 +0900

    dev/make it better

--- Code.js
+++ Code.js
@@ -1,39 +1,27 @@
 const isValid = (s) => {
-    let purification = [];
+    let s_Number = [];
+    //括弧を抽出し、それぞれを数字に変換して格納。
     for(let i in s){
         switch(s[i]){
-            case '(':
-                purification.push(1); break;
-            case ')':
-                purification.push(-1); break;
-            case '[':
-                purification.push(2); break;
-            case ']':
-                purification.push(-2); break;
-            case '{':
-                purification.push(3); break;
-            case '}':
-                purification.push(-3); break;
-            default:
-                continue;
+            case '(': s_Number.push(1); break;
+            case ')': s_Number.push(-1); break;
+            case '[': s_Number.push(2); break;
+            case ']': s_Number.push(-2); break;
+            case '{': s_Number.push(3); break;
+            case '}': s_Number.push(-3); break;
+            default: break;
         }
     }
-    for(let i=0;i<s.length/2;i++) if(i%2 == purification.lastIndexOf(-purification[i])%2) return false;
+    //括弧のルールが違えばその時点でfalseを返す。
+    for(let i=0;i<s.length/2;i++) if(i%2 == s_Number.lastIndexOf(-s_Number[i])%2) return false;
     
     return true;
 };
-    
-    let s="()";
-    console.log(isValid(s)); 
-    
-    s="([]){}"
-    console.log(isValid(s)); 
-    
-    s="([]){[{}][}]"
-    console.log(isValid(s)); 
 
-    s="({{)}}";
-    console.log(isValid(s));
-     
+let s = '()';
+console.log(isValid(s)); 
 
+s = '({)}'
+console.log(isValid(s)); 
     
+
