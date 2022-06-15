c308a48377f67c53eee6e31616f4b815da272993
yama <s256282@wakayama-u.ac.jp>
2022-06-15 19:39:57 +0900

    dev/more less

--- Code.js
+++ Code.js
@@ -1,42 +1,39 @@
 const isValid = (s) => {
-    let purification = []
+    let purification = [];
     for(let i in s){
-        if(s[i] == '('){
-            purification.push(1)
-        }else if(s[i] == ')'){
-            purification.push(-1)
-        }else if(s[i] == '['){
-            purification.push(2)
-        }else if(s[i] == ']'){
-            purification.push(-2)
-        }else if(s[i] == '{'){
-            purification.push(3)
-        }else if( s[i] == '}'){
-            purification.push(-3)
+        switch(s[i]){
+            case '(':
+                purification.push(1); break;
+            case ')':
+                purification.push(-1); break;
+            case '[':
+                purification.push(2); break;
+            case ']':
+                purification.push(-2); break;
+            case '{':
+                purification.push(3); break;
+            case '}':
+                purification.push(-3); break;
+            default:
+                continue;
         }
     }
-    for(let i=0;i<s.length/2;i++){
-        if(i%2 == purification.lastIndexOf(-purification[i])%2){
-            return false
-        }
-    }
-    // console.log(String.fromCharCode('('.charCodeAt(0) + 1))
-    console.log(')'.charCodeAt(0))
-
-
-    return true
-    };
+    for(let i=0;i<s.length/2;i++) if(i%2 == purification.lastIndexOf(-purification[i])%2) return false;
+    
+    return true;
+};
     
     let s="()";
     console.log(isValid(s)); 
     
     s="([]){}"
     console.log(isValid(s)); 
-    s="([]){}"
+    
+    s="([]){[{}][}]"
     console.log(isValid(s)); 
 
-    s="({{)}}"
-    console.log(isValid(s))
+    s="({{)}}";
+    console.log(isValid(s));
      
 
     
